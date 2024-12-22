55f42929cf2fdabf902cd5b9ecee1e50bbc26d2e
Twoje Imię <aleksander.malicki@cyfroweszkoly.pl>
2024-12-22 13:49:33 +0100

    dodanie klasy player

---
+++ sportowy_manager/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Player
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int Score { get; private set; }
+
+    public Player(string name, string position, int score)
+    {
+        Name = name;
+        Position = position;
+        Score = score;
+    }
+
+    public void UpdateScore(int newScore)
+    {
+        Score = newScore;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Position: {Position}, Score: {Score}";
+    }
+}
+
+
